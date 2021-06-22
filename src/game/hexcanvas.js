bdaf8d8485ca5b45f7225a18391cc2045b2121ab
Craig Katsube <craig.katsube@mail.utoronto.ca>
2021-06-21 23:11:39 -0400

    added hex-drawing canvas

---
+++ src/game/hexcanvas.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+class Canvas extends React.Component {
+
+    state = {
+        hexSize: 50,
+        hexOrigin: {x: 100, y: 130},
+        canvasSize: {canvasWidth: 800, canvasHeight: 600}
+    }
+
+    componentDidMount() {
+        const { canvasWidth, canvasHeight } = this.state.canvasSize;
+        this.canvashex.width = canvasWidth;
+        this.canvashex.height = canvasHeight;
+        this.drawHex(this.canvashex, {x: 50, y: 50})
+    }
+
+    drawHex(canvasID, center) {
+        for (let i = 0; i < 6; i++) {
+            let start = this.getHexCornerCord(center, i);
+            let end = this.getHexCornerCord(center, i + 1);
+            this.drawLine(canvasID, {x: start.x, y: start.y}, {x: end.x, y: end.y});
+        }
+    }
+
+    getHexCornerCord(center, i) {
+        let angle_deg = 60 * i - 30;
+        let angle_rad = Math.PI / 180 * angle_deg;
+        let x = center.x + this.state.hexSize * Math.cos(angle_rad);
+        let y = center.y + this.state.hexSize * Math.sin(angle_rad);
+        return this.Point(x, y);
+    }
+
+    Point(x, y) {
+        return {x: x, y: y}
+    }
+
+    drawLine(canvasID, start, end) {
+        const ctx = canvasID.getContext('2d');
+        ctx.beginPath();
+        ctx.moveTo(start.x, start.y);
+        ctx.lineTo(end.x, end.y);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    render() {
+        return (
+            <div>
+                <canvas ref={canvashex => this.canvashex = canvashex}/>
+            </div>
+        )
+    }
+
+
+}
+
+export {Canvas as default}
